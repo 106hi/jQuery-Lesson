ff2bcd2273d67cbd7231a3475aa3705ba489593d
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-09 13:32:48 +0000

    [Add]アニメーション追加

--- app.js
+++ app.js
@@ -3,9 +3,18 @@
   // $('body').html('<h1>Hello jQuery!!<?h1>');
 // });
 
+// $(function(){
+  // $('.box1').css({
+    // 'background-color':'#0000ff',
+    // 'height':'100px'
+  // });
+// });
+
 $(function(){
-  $('.box1').css({
-    'background-color':'#0000ff',
-    'height':'100px'
+  $('.box1').slideDown(1500, function(){
+    $('.box1').css({'background-color':'#0000ff',
+    'height':'100px',
+    'width':'200px'
+    }).slideUp(1500);
   });
 });
