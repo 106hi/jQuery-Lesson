e584423a1e6dcdbcfe85d5cc2bc00e4019ddf4e1
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-09 10:29:13 +0000

    [Add]5章、div要素のスタイル変更

---
+++ app.js
@@ -0,0 +1,11 @@
+// $(function(){でもいい
+// $(document).ready(function(){
+  // $('body').html('<h1>Hello jQuery!!<?h1>');
+// });
+
+$(function(){
+  $('.box1').css({
+    'background-color':'#0000ff',
+    'height':'100px'
+  });
+});
