6ad5712d30825df0851ca603831db511bf509371
106hi <hiro3428shs96kzsi@gmail.com>
2023-05-09 13:56:39 +0000

    [Add]マウスオーバー、マウスアウト、マウスクリック

--- app.js
+++ app.js
@@ -10,11 +10,32 @@
   // });
 // });
 
+// アニメーション
+// $(function(){
+  // $('.box1').slideDown(1500, function(){
+    // $('.box1').css({'background-color':'#0000ff',
+    // 'height':'100px',
+    // 'width':'200px'
+    // }).slideUp(1500);
+  // });
+// });
+
+// マウスオーバー、マウスアウト
+// $(function(){
+  // $('.box1').mouseover(function(){
+    // $('.box1').addClass('box1-ext');
+  // });
+  // $('.box1').mouseout(function(){
+    // $('.box1').removeClass('box1-ext');
+  // });
+// });
+
+// マウスクリック
 $(function(){
-  $('.box1').slideDown(1500, function(){
-    $('.box1').css({'background-color':'#0000ff',
-    'height':'100px',
-    'width':'200px'
-    }).slideUp(1500);
+  $('.box1').on('click',function(){
+    $('.box1').addClass('box1-ext');
+  });
+  $('.box1').mouseout(function(){
+    $('.box1').removeClass('box1-ext');
   });
 });
