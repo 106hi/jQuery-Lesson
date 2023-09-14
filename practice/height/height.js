04513c4f5b865f97d1bd7de792f0160387894b71
106hi <hiro3428shs96kzsi@gmail.com>
2023-09-14 08:55:57 +0000

    Change js from height to width

--- practice/height/height.js
+++ practice/height/height.js
@@ -1,6 +1,6 @@
 (function($){
   $(document).ready(function(){
-    var result = $("#element").height();
+    var result = $("#element").width();
     $("#result").html(result); //結果出力用
   });
 })(jQuery);
