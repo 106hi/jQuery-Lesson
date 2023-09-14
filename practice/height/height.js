062ff4212684ac49903d18da7ecf34de02d0bc8d
106hi <hiro3428shs96kzsi@gmail.com>
2023-09-14 09:13:26 +0000

    Change the display of height, width, and background-color of height.html and height.js

--- practice/height/height.js
+++ practice/height/height.js
@@ -1,7 +1,13 @@
 (function($){
   $(document).ready(function(){
+    $("#element").height("270px");
     $("#element").width("100px");
-    var result = $("#element").width();
-    $("#result").html(result); //結果出力用
+
+    var result1 = $("#element").height();
+    $("#result1").html(result1);
+    var result2 = $("#element").width();
+    $("#result2").html(result2); //結果出力用
+    var result3 = $("#element").css("background-color");
+    $("#result3").html(result3);
   });
 })(jQuery);
