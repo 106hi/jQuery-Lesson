cde9965657e5da2403ddcc60c6f253abfb93c235
106hi <hiro3428shs96kzsi@gmail.com>
2023-09-20 14:42:46 +0000

    Change from = to != into line 5 of main.js

--- practice/data-name/main.js
+++ practice/data-name/main.js
@@ -1,7 +1,8 @@
-(function($){
+(function($) {
   $(document).ready(function() {
+    let $selected = [];
     // セレクタ
-    $selected = $("[data-name='brian']");
+    $selected = $("[data-name!='brian']");
     // マッチした要素にスタイルを付与する
     $selected.each(function(index) {
       $(this).addClass("bingo");
