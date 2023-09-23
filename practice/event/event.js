288ab17d006634dc4aa62efcec7a12576f1793b2
106hi <hiro3428shs96kzsi@gmail.com>
2023-09-23 08:13:45 +0000

    Change event into event.js

--- practice/event/event.js
+++ practice/event/event.js
@@ -1,8 +1,16 @@
 (function($){
   $(function(){
+    function log(text) {
+      $('#log').append($('<li />').text(text));
+    }
     // ここにイベント名を書く
-    $('#button').on('click', function () {
-      alert('イベントが発生しました');
-    });
+    $('#button').on('click', function (event) {
+      $('#log').empty();
+      log('currentTarget =' + event.currentTarget);
+      log('target        =' + event.target);
+      log('pageX         =' + event.pageX);
+      log('pageY         =' + event.pageY);
+      log('altKey        =' + event.altKey);
+    })
   });
 })(jQuery);
