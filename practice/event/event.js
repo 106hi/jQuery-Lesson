50c7b5cdd7a8f48f735551115d0272e08214de33
106hi <hiro3428shs96kzsi@gmail.com>
2023-09-24 15:07:17 +0000

    Change function into event.js

--- practice/event/event.js
+++ practice/event/event.js
@@ -1,16 +1,7 @@
 (function($){
   $(function(){
-    function log(text) {
-      $('#log').append($('<li />').text(text));
-    }
-    // ここにイベント名を書く
-    $('#button').on('click', function (event) {
-      $('#log').empty();
-      log('currentTarget =' + event.currentTarget);
-      log('target        =' + event.target);
-      log('pageX         =' + event.pageX);
-      log('pageY         =' + event.pageY);
-      log('altKey        =' + event.altKey);
-    })
+    $('#button').one('click', function(){
+      alert('clickされました');
+    });
   });
 })(jQuery);
