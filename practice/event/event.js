837bc608473bee8aa16bf2596966124af7ea7fe0
106hi <hiro3428shs96kzsi@gmail.com>
2023-09-27 13:25:44 +0000

    Change active button into event.js

--- practice/event/event.js
+++ practice/event/event.js
@@ -1,7 +1,15 @@
 (function($){
   $(function(){
-    $('#button').one('click', function(){
-      alert('clickされました');
+    function logEvent(event) {
+      $('#log').prepend($('<li />').text(event.type + 'が発生しました'));
+    }
+    $(document).on('click', '#button:not(.active)', function(event){
+      logEvent(event);
+      $(event.target).addClass('active');
+    });
+    $(document).on('click', '#button.active', function(event){
+      logEvent(event);
+      $(event.target).removeClass('active');
     });
   });
 })(jQuery);
